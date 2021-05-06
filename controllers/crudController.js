dc9077e74e2ca7efacf660374c1e89488e7013a4
Isaac Kamula <kamulaisaac15@gmail.com>
2021-05-06 15:06:43 +0300

    ready for deployment

--- controllers/crudController.js
+++ controllers/crudController.js
@@ -1,7 +1,7 @@
 const { all } = require('../app');
 const Crud = require('../models/crudModel');
 // create new data/payload function
-exports.createPayload = async(req, res, next) => {
+exports.createPayload = async(req, res) => {
     try {
         const newData = await Crud.create(req.body);
         res.status(200).json({
@@ -16,34 +16,67 @@ exports.createPayload = async(req, res, next) => {
     }
 };
 // get all data from database
-exports.getAllData = async(req, res, next) => {
+exports.getAllData = async(req, res) => {
     try {
-        const alldata = await Crud.find()
+        const alldata = await Crud.find();
         res.status(200).json({
             message: 'success',
             count: alldata.length,
             data: {
-                alldata
-            }
+                alldata,
+            },
         });
     } catch (err) {
         res.status(400).json({
             message: 'fail',
-            error: "no data",
+            error: 'no data',
         });
     }
 };
-// update data
-exports.updateData = (req, res, next) => {
-    res.status(200).json({
-        message: 'success',
-        data: 'update data',
-    });
+exports.getSingleItem = async(req, res) => {
+    try {
+        const data = await Crud.findById(req.params.id);
+        res.status(204).json({
+            message: 'success',
+            data: {
+                data,
+            },
+        });
+    } catch (err) {}
 };
-// delete data
-exports.deleteData = (req, res, next) => {
-    res.status(200).json({
-        message: 'success',
-        data: 'delete data',
-    });
+// update data
+exports.updateData = async(req, res) => {
+    try {
+        // update and return the updated data to client
+        const updatedData = await Crud.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(200).json({
+            message: 'success',
+            data: {
+                updatedData,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: 'fail',
+            error: 'not updated',
+        });
+    }
+};
+// delete data and return response
+exports.deleteData = async(req, res) => {
+    try {
+        await Crud.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            message: 'success',
+            data: null,
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: 'fail',
+            error: 'delete failed',
+        });
+    }
 };
--- routes/crudRouter.js
+++ routes/crudRouter.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const crudController = require('../controllers/crudController');
 
-
 const router = express.Router();
 
 // controller routes
-router.route('/').get(crudController.getAllData).post(crudController.createPayload);
+router
+    .route('/')
+    .get(crudController.getAllData)
+    .post(crudController.createPayload);
 router
     .route('/:id')
+    .get(crudController.getSingleItem)
     .patch(crudController.updateData)
     .delete(crudController.deleteData);
 module.exports = router;
