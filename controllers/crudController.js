853e4542d2439299a3681108be5ebb8c9bd159c6
Isaac Kamula <kamulaisaac15@gmail.com>
2021-05-06 13:58:22 +0300

    create and get all data functions

--- controllers/crudController.js
+++ controllers/crudController.js
@@ -1,21 +1,46 @@
-exports.createPayload = (req, res, next) => {
-    res.status(200).json({
-        message: 'success',
-        data: 'create item',
-    });
+const { all } = require('../app');
+const Crud = require('../models/crudModel');
+// create new data/payload function
+exports.createPayload = async(req, res, next) => {
+    try {
+        const newData = await Crud.create(req.body);
+        res.status(200).json({
+            message: 'success',
+            data: newData,
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: 'fail',
+            error: 'invalid data',
+        });
+    }
 };
-exports.getAll = (req, res, next) => {
-    res.status(200).json({
-        message: 'success',
-        data: 'get all items',
-    });
+// get all data from database
+exports.getAllData = async(req, res, next) => {
+    try {
+        const alldata = await Crud.find()
+        res.status(200).json({
+            message: 'success',
+            count: alldata.length,
+            data: {
+                alldata
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            message: 'fail',
+            error: "no data",
+        });
+    }
 };
+// update data
 exports.updateData = (req, res, next) => {
     res.status(200).json({
         message: 'success',
         data: 'update data',
     });
 };
+// delete data
 exports.deleteData = (req, res, next) => {
     res.status(200).json({
         message: 'success',
