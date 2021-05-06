ba35dea725b38bd4694f06c8afc3cd1befb014fa
Isaac Kamula <kamulaisaac15@gmail.com>
2021-05-06 13:59:03 +0300

    updated app structure

--- routes/crudRouter.js
+++ routes/crudRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const crudController = require('../controllers/crudController');
 
+
 const router = express.Router();
 
 // controller routes
-router.route('/').get(crudController.getAll).post(crudController.createPayload);
+router.route('/').get(crudController.getAllData).post(crudController.createPayload);
 router
     .route('/:id')
     .patch(crudController.updateData)
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const app = require('./app');
+dotenv.config({ path: './config.env' });
+// database url path
+const database = process.env.DATABASE_URL.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+// connect to db
+mongoose.connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(conn => {
+
+    console.log('conected to db')
+})
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`app listening on ${port}`)
-})
+    console.log(`app listening on ${port}`);
+});
